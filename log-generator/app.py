a4b15a322eb29017daac9a521e2635e249c61616
Bruna Alves <brunawsiqueira@gmail.com>
2025-08-14 22:30:07 -0700

    Configuring Procfile and flask deploy

--- log-generator/app.py
+++ log-generator/app.py
@@ -49,6 +49,7 @@ def process_csvs():
         download_name='generated_logs.zip'
     )
   
-if __name__ == '__main__':  
-    app.run(port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
 
